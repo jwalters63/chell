e38093d0440dace9963b95f8e0a92d0f814e2fa1
jwalters63 <jeffwalters@uamv.edu.ni>
2026-07-05 01:35:28 -0600

    Pequeña validación

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-struct REPL {
-    char buffer[1024]; // Buffer que guarda las cadenas de string
+struct REPL {                                                 // Struct para variables globales
+    char buffer[1024];                                        // Buffer que guarda las cadenas de string
 };
 
 int main(void) {
-    struct REPL repl; // Crear el objeto del struct REPL
+    struct REPL repl;                                         // Crear el objeto del struct REPL
+    char *fgets_status;                                       // Corroborar el estado de fgets
 
-    while (true) { // REPL, la unica forma de matar al shell es con CTRL + C
-        fputs("-> ", stdout); // Prompt para el usuario
-        fgets(repl.buffer, sizeof(repl.buffer), stdin);
+    while (true) {                                            // REPL, la unica forma de matar al shell es con CTRL + C
+        do {
+            fputs("-> ", stdout);                           // Prompt para el usuario
+            fgets_status = fgets(repl.buffer, sizeof(repl.buffer), stdin);
+        } while (fgets_status != NULL);
+
+        break;
     }
+
+    return -1;
 }
